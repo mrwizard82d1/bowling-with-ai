aaecc5313dcdc5a985317493cfee2bbe4053c89b
Larry Jones <larry.jones@continuous.com>
2026-04-23 15:26:30 -0500

    Green unit (and acceptance) tests

--- src/BowlingWithAI.Domain/Game.cs
+++ src/BowlingWithAI.Domain/Game.cs
@@ -2,12 +2,16 @@ namespace BowlingWithAI.Domain;
 
 public class Game
 {
+    private readonly int[] _rolls = new int[21];
+    private int _currentRoll;
+
     public void Roll(int pins)
     {
+        _rolls[_currentRoll++] = pins;
     }
 
     public int Score()
     {
-        return -1;
+        return _rolls.Sum();
     }
 }
