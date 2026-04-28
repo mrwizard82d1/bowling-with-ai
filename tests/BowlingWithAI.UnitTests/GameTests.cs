e9b69beccb7c30401f76bcb0183ec875f7e6fdbe
Larry Jones <larry.jones@continuous.com>
2026-04-28 12:15:24 -0500

    Failing unit test
    Spare followed by a non-gutter roll.

--- tests/BowlingWithAI.UnitTests/GameTests.cs
+++ tests/BowlingWithAI.UnitTests/GameTests.cs
@@ -14,4 +14,18 @@ public class GameTests
 
         Assert.Equal(0, game.Score());
     }
+
+    [Fact]
+    public void Spare_AddsNextRollAsBonus()
+    {
+        var game = new Game();
+
+        game.Roll(5);
+        game.Roll(5); // spare
+        game.Roll(3); // bonus roll
+        for (int i = 0; i < 17; i++)
+            game.Roll(0);
+
+        Assert.Equal(16, game.Score());
+    }
 }
